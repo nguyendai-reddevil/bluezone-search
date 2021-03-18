cd3fbbfa7787993e83e6fe24561751f5d6be6eac
khacchung <ochung3@gmail.com>
2021-03-18 17:57:04 +0700

    add search keyword

--- app/main/components/SearchScreen/component/ItemKeyword.js
+++ app/main/components/SearchScreen/component/ItemKeyword.js
@@ -1,45 +1,53 @@
-import React,{ memo } from 'react';
-import { View,Text,Image,TouchableOpacity } from 'react-native';
-import {MSCALE} from '../Reponsive';
+import React, { memo } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { MSCALE } from '../Reponsive';
 
-const ItemSearch = ({item,onPress}) => {
-    console.log('itetmemtemtmetetmemtmemte',item)
-    return(
-        <TouchableOpacity 
-        onPress={onPress}
-        style={{flexDirection:'row',
-        alignItems:'center',
-        borderBottomColor:'#E0E0E0',
-        height:MSCALE(71),borderBottomWidth:1}}>
+const ItemSearch = ({ item, onPress }) => {
+    const onSelectValue = () => {
+        if (onPress) {
+            onPress(item)
+        }
+    }
+
+    return (
+        <TouchableOpacity
+            onPress={onSelectValue}
+            style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                borderBottomColor: '#E0E0E0',
+                height: MSCALE(71), borderBottomWidth: 1
+            }}>
             <Image
                 source={require('../asset/lock.png')}
                 resizeMode={'contain'}
                 width={MSCALE(18)}
                 height={MSCALE(18)}
-                style={{width:MSCALE(18),height:MSCALE(18)}}
+                style={{ width: MSCALE(18), height: MSCALE(18) }}
             />
             <Text
-            numberOfLines={1}
-            style={{paddingLeft:MSCALE(28),fontSize:MSCALE(17), 
-                fontFamily: 'Roboto-Medium',fontWeight:'400'}}>
-                {item.content}
-            </Text>
-            <View style={{position:'absolute',right:0,backgroundColor:'white'}}>
-            <Image
-                source={require('../asset/choose.png')}
-                resizeMode={'contain'}
-                width={MSCALE(18)}
-                height={MSCALE(18)}
+                numberOfLines={1}
                 style={{
-                    // position:'absolute',
-                    // right:MSCALE(29.68),
-                    marginRight:MSCALE(29.68),
-                    marginLeft:MSCALE(15),
-                    width:MSCALE(18),
-                    height:MSCALE(18)}}
-            />
+                    paddingLeft: MSCALE(28), fontSize: MSCALE(17),
+                    fontFamily: 'Roboto-Medium', fontWeight: '400'
+                }}>
+                {item?.keyword}
+            </Text>
+            <View style={{ position: 'absolute', right: 0, backgroundColor: 'white' }}>
+                <Image
+                    source={require('../asset/choose.png')}
+                    resizeMode={'contain'}
+                    width={MSCALE(18)}
+                    height={MSCALE(18)}
+                    style={{
+                        marginRight: MSCALE(29.68),
+                        marginLeft: MSCALE(15),
+                        width: MSCALE(18),
+                        height: MSCALE(18)
+                    }}
+                />
             </View>
-           
+
         </TouchableOpacity>
     )
 }
